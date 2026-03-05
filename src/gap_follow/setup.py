de61be349ee381aa62245c677557a191a396ffce
Ederic Oytas <edericoytas@gmail.com>
2026-03-04 18:14:55 -0800

    Replace email with YOUREMAILHERE

--- src/gap_follow/setup.py
+++ src/gap_follow/setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="root",
-    maintainer_email="edericoytas@gmail.com",
+    maintainer_email="YOUREMAILHERE",
     description="TODO: Package description",
     license="TODO: License declaration",
     tests_require=["pytest"],
